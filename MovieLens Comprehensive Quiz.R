a74a3ade58f5b42a7c2c0cffb9cd30fa76b85683
Veronicaots <79258965+Veronicaots@users.noreply.github.com>
2021-12-09 15:02:06 +0000

    Add files via upload

--- MovieLens Comprehensive Quiz.R
+++ MovieLens Comprehensive Quiz.R
@@ -1,3 +1,9 @@
+## Made Ots
+## Movie Recimmendation System
+## HarvardX: PH125.9x - Capstone Project
+## https://github.com/Veronicaots/Harvard-CAPSTONE
+
+
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
 if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
@@ -40,4 +46,54 @@ removed <- anti_join(temp, validation)
 edx <- rbind(edx, removed)
 
 rm(dl, ratings, movies, test_index, temp, movielens, removed)
+
+# Head
+head(edx) %>%
+  print.data.frame()
+
+# Users & Movies
+summary(edx)
+
+install.packages("DescTools")
+library(DescTools)
+
+# Rows & Columns in the dataset
+dim(edx)
+
+# How many zeros were given as ratings in the edx dataset?
+edx %>% filter(rating == 0) %>% nrow()
+
+# How many threes were given as ratings in the edx dataset?
+edx %>% filter(rating == 3) %>% nrow()
+
+# How many different movies are in the edx dataset?
+edx %>% group_by(movieId) %>% summarize(count = n())
+
+# How many different users are in the edx dataset?
+edx %>% group_by(userId) %>% summarize(count = n())
+
+# How many movie ratings are in each of the following genres in the edx
+#  dataset?
+edx %>% filter(genres %like% "%Drama%") %>% summarize(n())
+edx %>% filter(genres %like% "%Comedy%") %>% summarize(n())
+edx %>% filter(genres %like% "%Thriller%") %>% summarize(n())
+edx %>% filter(genres %like% "%Romance%") %>% summarize(n())
+
+# Which movie has the most ratings?
+edx %>% group_by(title) %>%
+  summarize(n_ratings = n()) %>%
+  arrange(desc(n_ratings))
+
+# What are the five most given ratings in order from most to least?
+edx %>% group_by(rating) %>%
+  summarize(n_ratings = n()) %>%
+  arrange(desc(n_ratings))
+
+# True or False, In general, half star ratings are less common than whole star ratings (e.g., there are fewer ratings of 3.5 than there are ratings of 3 or 4, etc.).
+edx %>% group_by(rating) %>%
+  summarize(n_ratings = n()) %>%
+  arrange(desc(n_ratings))
+
+
+
 
